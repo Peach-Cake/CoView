46ffc602e8b08b5e2e9325f508eff88a63ccf67b
b1700161 <52756287+b1700161@users.noreply.github.com>
2020-10-12 23:19:19 +0800

    Filter by Status added + bit of cleanup

--- CoViewJS.js
+++ CoViewJS.js
@@ -1,36 +1,36 @@
 //Filter Modal box
 // Get the modal
-var modal = document.getElementById("filterModal");
+var filterModal = document.getElementById("filterModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("filterButton");
+var filterBtn = document.getElementById("filterButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var filterSpan = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+filterBtn.onclick = function() {
+  filterModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+filterSpan.onclick = function() {
+  filterModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == filterModal) {
+    filterModal.style.display = "none";
   }
 }
 //filterForm
-var filter = document.getElementById('filterForm')
-var applyFilterBtn = document.getElementById("applyFilter");
-applyFilterBtn.onclick = function() {
-  var unchecked = [];
-  var status = document.getElementsByName('patientStatus');
-  for (var i = 0; i < status.length; i++) {
+var filter = document.getElementById('filterForm');
+
+function getPatientTypeFilter(){
+  let unchecked = [];
+  let status = document.getElementsByName('patientStatus');
+  for (let i = 0; i < status.length; i++) {
     if (status[i].checked == false) {
       unchecked.push(status[i].labels[0].innerHTML);
     }
@@ -38,18 +38,40 @@ applyFilterBtn.onclick = function() {
   if(unchecked.length == 5){
     unchecked = [];
   }
-  var table = document.getElementById("reports");
-  var rows = table.rows;
-  for (var i = 0; i < rows.length; i++) {
+  return unchecked;
+}
+function getReportStatusFilter(){
+  let status = document.getElementsByName('status');
+  if(status[0].checked == true){
+    return "Pending";
+  }
+  else if(status[1].checked == true){
+    return "Complete";
+  }
+  else if(status[2].checked == true){
+    return null;
+  }
+}
+var applyFilterBtn = document.getElementById("applyFilter");
+applyFilterBtn.onclick = function() {
+  let unchecked = getPatientTypeFilter();
+  let status = getReportStatusFilter();
+  let table = document.getElementById("reports");
+  let rows = table.rows;
+  //unhides the hidden things so they don't remain hidden
+  for (let i = 0; i < rows.length; i++) {
     rows[i].style.display = '';
   }
-  for (var n = 1; n < (rows.length); n++) {
-    //var rowStatus = true;
-    for (var i = 0; i < unchecked.length; i++) {
-      if (unchecked[i] == rows[n].getElementsByTagName("TD")[2].innerHTML) {
-        //rowStatus = false;
-        rows[n].style.display = 'none';
+  //applys the filter
+  for (let n = 1; n < (rows.length); n++) {
+    let currentRow = rows[n];
+    for (let i = 0; i < unchecked.length; i++) {
+      if (unchecked[i] == currentRow.getElementsByTagName("TD")[2].innerHTML) {
+        currentRow.style.display = 'none';
       }
+      }
+      if (status != null && status != currentRow.getElementsByTagName("TD")[5].innerHTML) {
+        currentRow.style.display = 'none';
     }
   }
 
